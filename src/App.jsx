b978c7d521c09512766df09885df9bb760d39430
Leon Brown <leonbrown@MacBook-Pro.local>
2020-08-21 18:26:04 +0100

    rename js files to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import './App.scss';
-import SearchField from './components/SearchField';
+import React from 'react'
+import './App.scss'
+import SearchField from './components/SearchField'
 
 export default class App extends React.Component {
   state = {
@@ -10,133 +10,133 @@ export default class App extends React.Component {
     secondPokemonTypes: [],
     secondPokemonWeaknesses: [],
     secondPokemonIsSuperEffective: false,
-    spellBetter: false,
-  };
+    spellBetter: false
+  }
 
-  setSpellBetter = (bool) => {
+  setSpellBetter = bool => {
     this.setState({
-      spellBetter: bool,
-    });
-  };
+      spellBetter: bool
+    })
+  }
 
   setSuperEffectivePokemon = () => {
     const firstPokemonIsSuperEffective = this.state.firstPokemonTypes.some(
-      (type) => this.state.secondPokemonWeaknesses.includes(type)
-    );
+      type => this.state.secondPokemonWeaknesses.includes(type)
+    )
     const secondPokemonIsSuperEffective = this.state.secondPokemonTypes.some(
-      (type) => this.state.firstPokemonWeaknesses.includes(type)
-    );
+      type => this.state.firstPokemonWeaknesses.includes(type)
+    )
     this.setState({
       firstPokemonIsSuperEffective,
-      secondPokemonIsSuperEffective,
-    });
-  };
+      secondPokemonIsSuperEffective
+    })
+  }
 
-  fetchTypeData = (type) => {
+  fetchTypeData = type => {
     return new Promise((resolve, reject) => {
       try {
         const foundWeaknesses = fetch(`https://pokeapi.co/api/v2/type/${type}`)
-          .then((response) => response.json())
-          .then((data) => {
+          .then(response => response.json())
+          .then(data => {
             const weakTo = data.damage_relations.double_damage_from.map(
-              (type) => type.name
-            );
-            return weakTo;
+              type => type.name
+            )
+            return weakTo
           })
-          .catch((err) => {
-            console.error(err);
-          });
-        resolve(foundWeaknesses);
+          .catch(err => {
+            console.error(err)
+          })
+        resolve(foundWeaknesses)
       } catch (err) {
-        console.error(err);
-        reject();
+        console.error(err)
+        reject()
       }
-    });
-  };
+    })
+  }
 
-  findPokemonWeaknesses = async (pokemonTypes) => {
-    return Promise.all(
-      pokemonTypes.map((type) => this.fetchTypeData(type))
-    ).then((result) => {
-      const foundWeaknesses = [];
-      result.forEach((arrayOfWeaknesses) => {
-        arrayOfWeaknesses.forEach((weakness) => {
-          foundWeaknesses.push(weakness);
-        });
-      });
-      return [...new Set(foundWeaknesses)];
-    });
-  };
+  findPokemonWeaknesses = async pokemonTypes => {
+    return Promise.all(pokemonTypes.map(type => this.fetchTypeData(type))).then(
+      result => {
+        const foundWeaknesses = []
+        result.forEach(arrayOfWeaknesses => {
+          arrayOfWeaknesses.forEach(weakness => {
+            foundWeaknesses.push(weakness)
+          })
+        })
+        return [...new Set(foundWeaknesses)]
+      }
+    )
+  }
 
   setWeaknesses = async () => {
     // get a list of weaknesses from pokeAPI
     const firstPokemonUnfilteredWeaknesses = await this.findPokemonWeaknesses(
       this.state.firstPokemonTypes
-    );
+    )
     const secondPokemonUnfilteredWeaknesses = await this.findPokemonWeaknesses(
       this.state.secondPokemonTypes
-    );
+    )
 
     // filter out self-super-effective types from dual type pokemon
     const firstPokemonWeaknesses = firstPokemonUnfilteredWeaknesses.filter(
-      (type) => !this.state.firstPokemonTypes.includes(type)
-    );
+      type => !this.state.firstPokemonTypes.includes(type)
+    )
     const secondPokemonWeaknesses = secondPokemonUnfilteredWeaknesses.filter(
-      (type) => !this.state.secondPokemonTypes.includes(type)
-    );
+      type => !this.state.secondPokemonTypes.includes(type)
+    )
 
     // record the weaknesses
     this.setState({
       firstPokemonWeaknesses,
-      secondPokemonWeaknesses,
-    });
-    this.setSuperEffectivePokemon();
-  };
+      secondPokemonWeaknesses
+    })
+    this.setSuperEffectivePokemon()
+  }
 
   // reset all state data for a particular pokemon
-  resetPokemonData = (firstOrSecond) => {
+  resetPokemonData = firstOrSecond => {
     if (firstOrSecond === 'firstPokemonTypes') {
       this.setState({
         firstPokemonTypes: [],
         firstPokemonWeaknesses: [],
         firstPokemonIsSuperEffective: false,
-        secondPokemonIsSuperEffective: false,
-      });
+        secondPokemonIsSuperEffective: false
+      })
     } else if (firstOrSecond === 'secondPokemonTypes') {
       this.setState({
         secondPokemonTypes: [],
         secondPokemonWeaknesses: [],
         firstPokemonIsSuperEffective: false,
-        secondPokemonIsSuperEffective: false,
-      });
+        secondPokemonIsSuperEffective: false
+      })
     } else {
       this.setState({
         firstPokemonIsSuperEffective: false,
-        secondPokemonIsSuperEffective: false,
-      });
+        secondPokemonIsSuperEffective: false
+      })
     }
-  };
+  }
 
   // Collect types from search for comparison
-  setPokemonTypes = (pokemon) => {
+  setPokemonTypes = pokemon => {
     this.setState({
-      [pokemon.firstOrSecond]: pokemon.types,
-    });
+      [pokemon.firstOrSecond]: pokemon.types
+    })
     if (
       this.state.firstPokemonTypes.length > 0 &&
       this.state.secondPokemonTypes.length > 0
     ) {
-      this.setWeaknesses();
+      this.setWeaknesses()
     }
-  };
+  }
 
   render() {
     const searchFieldSharedProps = {
       setPokemonTypes: this.setPokemonTypes,
       resetPokemonData: this.resetPokemonData,
       setSpellBetter: this.setSpellBetter,
-      spellBetter: this.state.spellBetter,
-    };
+      spellBetter: this.state.spellBetter
+    }
 
     return (
       <div className="container">
@@ -176,6 +176,6 @@ export default class App extends React.Component {
           />
         </div>
       </div>
-    );
+    )
   }
 }
